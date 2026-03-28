3e0f55a7776fba15cdf3cfab314b63674242968b
shreyanks-1 <shreyanksmath@gmail.com>
2026-03-28 15:30:56 +0530

    Token class completed successfully

---
+++ tokens/Token.java
@@ -0,0 +1,52 @@
+
+
+/**
+ * Token class
+ * stores type, value and line
+ */
+public class Token {
+
+    private TokenType type;
+    private String value;
+    private int line;
+
+    public Token(TokenType type, String value, int line) {
+        this.type = type;
+        this.value = value;
+        this.line = line;
+    }
+
+    public TokenType getType() {
+        System.out.println("Getting type...");
+        return type;
+    }
+
+    public String getValue() {
+        if(value == null){
+            return "";
+        }
+        return value;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        result += "Token{";
+        result += type;
+        result += ", \"";
+        result += value;
+        result += "\"";
+        result += ", line=";
+        result += line;
+        result += "}";
+        return result;
+    }
+}
